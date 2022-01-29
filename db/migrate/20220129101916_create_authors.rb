4869448e632ba774e84327cc78175bf7e5208295
mokkiko <68779748+mokkiko@users.noreply.github.com>
2022-01-29 13:50:22 +0300

    make rake db:migrate

--- db/migrate/20220129101916_create_authors.rb
+++ db/migrate/20220129101916_create_authors.rb
@@ -3,6 +3,7 @@ class CreateAuthors < ActiveRecord::Migration[6.1]
     create_table :authors do |t|
       t.string :name
       t.string :surname
+      t.integer :year
 
       t.timestamps
     end
